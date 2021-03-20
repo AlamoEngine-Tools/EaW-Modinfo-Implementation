c729d1be4eb996495a9d8935d67d6f1a0c0a0ac4
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2021-03-20 18:58:48 +0100

    Add public strong name key

--- src/EawModinfo/IsExternalInit.cs
+++ src/EawModinfo/IsExternalInit.cs
@@ -1,9 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("EawModinfo.Tests")]
-
-
-#if !NET
+﻿#if !NET
 // ReSharper disable once CheckNamespace
 namespace System.Runtime.CompilerServices
 {
